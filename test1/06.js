b74b1ebd503c6d2fd3f883c636af313b4029f602
ilona-fedorovich <iwonasowa93@gmail.com>
2025-03-16 19:03:37 +0300

    feat: add 6 and 7 tasks

--- test1/06.js
+++ test1/06.js
@@ -6,10 +6,33 @@
 если они считаются эквивалентными для целей уникальности, и false в
 противном случае. */
 
-function getObjArray (arr) {
+function getObjArray (array) {
 
-    let currentArray = arr;
-    let newArray = [];
+    let currentArray = array;
+    let newArray = currentArray.reduce((acc, item) => { 
+    let oldItem = acc.find(oldItem => oldItem.id === item.id);
+        if (oldItem) {
+        } 
+        else {
+        acc.push(item) 
+        }
+        return acc;
+        }, 
+        []);
 
     return newArray;
 }
+
+let idPersons = [
+    {id: 1, name: 'John', age: 28},
+    {id: 2, name: 'John', age: 26},
+    {id: 1, name: 'John', age: 26},
+    {id: 3, name: 'Doe', age: 36},
+    {id: 3, name: 'Patricia', age: 36}
+    ];
+
+console.log(getObjArray(idPersons));
+
+
+
+
