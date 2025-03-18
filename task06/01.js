2add6a44c512b45315d24367eeedc0fcfd522a88
ilona-fedorovich <iwonasowa93@gmail.com>
2025-03-18 15:17:03 +0300

    fix: edit the 01.js file

--- task06/01.js
+++ task06/01.js
@@ -1,9 +1,25 @@
-/* 1. Дана строка из четного количества цифр. Проверьте, что сумма первой половины цифр равняется 
-сумме второй половине цифр. Если это так - выведите 'да', в противном случае выведите 'нет'. */
+// /* 1. Дана строка из четного количества цифр. Проверьте, что сумма первой половины цифр равняется 
+// сумме второй половине цифр. Если это так - выведите 'да', в противном случае выведите 'нет'. */
 
-let numStr = '343415';
 
-if((+numStr[0] + +numStr[1] + +numStr[2]) === (+numStr[3] + +numStr[4] + +numStr[5]))
-    {console.log('true')}
-else 
-{console.log('false')};
+function checkNumbers(string) {
+    let stringNum = string;
+    string = +string;
+    let left = 0;
+    let right = 0;
+
+    for(let i = 0, j = stringNum.length-1; i < stringNum.length/2; i++, j--) {
+        left += Number(stringNum[i]); 
+        right += Number(stringNum[j]); 
+    }
+
+    if(left == right) {
+        return true;
+      }
+      else {
+        return false;
+      }
+
+  }
+  
+  console.log(checkNumbers('0550'));
