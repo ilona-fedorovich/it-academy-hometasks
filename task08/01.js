48fbad13ac0283ec48c3c89ac2d5f532aa2b89e9
ilona-fedorovich <iwonasowa93@gmail.com>
2025-04-07 18:39:17 +0300

    feat: add async function for 01 file

--- task08/01.js
+++ task08/01.js
@@ -3,6 +3,7 @@
 // С помощью Promise.race дождитесь загрузки первого сработавшего промиса и выведите результат его работы на экран.
 
 
+/*Решение задачи через промисы*/
 let promiseOne = new Promise(function(resolve) {
     setTimeout(() => resolve(1), 3000);
   });
@@ -12,7 +13,7 @@ let promiseOne = new Promise(function(resolve) {
   });
 
   let promiseThree = new Promise(function(resolve) {
-    setTimeout(() => resolve(3), 1000);
+    setTimeout(() => resolve(3), 1500);
   });
 
   Promise.race([promiseOne, promiseTwo, promiseThree])
@@ -20,3 +21,24 @@ let promiseOne = new Promise(function(resolve) {
     console.log(value);
   });
 
+/*Решение задачи через объявление синх. ф-и и внутри объявление тех же промисов*/
+  async function getNumber() {
+    let promiseOne = new Promise(function(resolve) {
+        setTimeout(() => resolve(1), 1000);
+      });
+    let promiseTwo = new Promise(function(resolve) {
+        setTimeout(() => resolve(2), 4000);
+      });
+    
+    let promiseThree = new Promise(function(resolve) {
+        setTimeout(() => resolve(3), 2000);
+      });
+    Promise.race([promiseOne, promiseTwo, promiseThree])
+      .then((value) => {
+        console.log(value);
+      });
+  }
+
+  getNumber()
+
+  /* Итого вывожу разными способами 2 цифры через заданный промежуток времени для наглядности (1, 3)*/
